f605749a9ce0662688fb68c26d66b57db0bddeb9
MyeonGyeong111 <154568076+MyeonGyeong111@users.noreply.github.com>
2024-01-08 18:51:17 +0900

    Create minesweeper.h

---
+++ minesweeper.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void _load_(string str1, string** a, string** b);
+
+bool _touch_(int x, int y, string output, string** array1, string** array2, string** array3, int& i4);
+
+void _display_(string** a, string str);
+
+void minesweeper(string commands, string output);
